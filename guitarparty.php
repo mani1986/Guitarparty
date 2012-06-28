df9deae67849c5c9e6e7d38142537d77f6343dcf
Mani G <mani.gudvardarson@gmail.com>
2012-06-28 16:52:54 +0000

    revision

--- guitarparty.php
+++ guitarparty.php
@@ -16,15 +16,27 @@ class Guitarparty
 	private $apiEndpoint = 'v2/';
 	
 	
-	public function makeRequest($endpoint)
+	private function makeRequest($endpoint, $type = 1, $data = null)
 	{
-		$endpoint = URL.$endpoint;
-		$header = array('Guitarparty-Api-Key: '.API_KEY.'');
+		$endpoint = $this->apiUri.$this->apiEndpoint.$endpoint;
+		$header = array('Content-Type: application/json',                                                                                
+						'Content-Length: ' . strlen($data),
+						'Guitarparty-Api-Key: '.API_KEY.'');
 		$curl = curl_init($endpoint);
+		if ($type == 2)
+		{
+			echo $data;
+			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $data);  
+		}
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		
 		$json = curl_exec($curl);
 		curl_close($curl);
+		echo '<pre>';
+		print_r(json_decode($json));
+		echo '</pre>';
 		return json_decode($json);
 	}
 	
@@ -43,6 +55,7 @@ class Guitarparty
 	
 	public function searchSongs($searchString)
 	{
+		$searchString = urlencode($searchString);
 		$songs = $this->makeRequest('songs/?query='.$searchString);
 		return $songs;
 	}
@@ -51,6 +64,33 @@ class Guitarparty
 	{
 		$artist = $this->makeRequest('artists/'.$artistId.'/');
 		return $artist;
+	}
+	
+	public function searchArtists($searchString)
+	{
+		$searchString = urlencode($searchString);
+		$songs = $this->makeRequest('artists/?query='.$searchString);
+		return $songs;
+	}
+	
+	public function getSongbooks()
+	{
+		$songbooks = $this->makeRequest('songbooks/');
+		$i = 0;
+		foreach($songbooks->objects as $songbook)
+		{
+			$i++;
+			$id = explode("/", $songbook->uri);
+			$songbooks[i][id] = $id[2];
+		}
+		return $songbooks;
+	}
+	
+	public function createSongbook($title, $description)
+	{
+		$data = array('title' => $title, 'description' => $description);
+		$songbook = $this->makeRequest('songbooks', 2, json_encode($data));
+		return $songbook;
 	}
 };
 
